e3576f9a53a4c2bd785a1f2db5a7e11daee62b90
yash1404 <yashe045@gmail.com>
2021-06-25 14:06:50 +0530

    Add files via upload
    This is cryptocurrency site..

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const opennav= ()=>{
+   document.getElementById("carryallnav").style.width="250px";
+   document.getElementById("carryallnav").style.display="block";
+}
+const closenav= ()=>{
+  document.getElementById("carryallnav").style.width="0px";
+  document.getElementById("carryallnav").style.display="none";
+}
